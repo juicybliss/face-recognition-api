5d208740829ed0bbb6d7fc64e15141fd332f9cc4
Elvia <etcamarena@gmail.com>
2021-09-09 11:57:08 -0700

    using script.js and planning endpoints

---
+++ script.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('this is working')
+})
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+})
+
+
+/*
+Endpoints
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT  --> updated user count
+
+*/
