b01e6b74f2a7a27abdb3ce6d21aede9c0c27db91
Elvia <etcamarena@gmail.com>
2021-09-10 22:29:36 -0700

    connected image counter endpoint to DB

--- server.js
+++ server.js
@@ -68,18 +68,13 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
-    let found = false;
-
-    database.users.forEach(user => {
-        if(user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
-    })
-    if (!found) {
-        res.status(404).json('not found!');
-    }
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+      res.json(entries[0])
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
 })
 
 
