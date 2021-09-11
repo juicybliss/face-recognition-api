ef3a303cea34167205357286f81647ac58af99af
Elvia <etcamarena@gmail.com>
2021-09-10 22:12:37 -0700

    Connected /profile/:id functionality to Database

--- server.js
+++ server.js
@@ -52,18 +52,18 @@ app.post('/register', (req, res) => {
 })
 
 app.get('/profile/:id', (req, res) => {
-    console.log('request params: ', req.params);
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id) {
-            found = true;
-            return res.json(user);
+    db.select('*').from('users').where({
+        id: id
+    })
+    .then(user => {
+        if(user.length) {
+            res.json(user[0]);
+        } else {
+            res.status(400).json("Not Found")
         }
     })
-    if (!found) {
-            res.status(404).json('not found!');
-    }
+    .catch(err => res.status(400).json('Error getting user'))
 })
 
 app.put('/image', (req, res) => {
