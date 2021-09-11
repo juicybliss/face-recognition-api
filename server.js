f4f453670488553692688f96dc70bdec9c66744b
Elvia <etcamarena@gmail.com>
2021-09-10 21:46:25 -0700

    Connected API to Database. Linked /register post path to Database

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex')
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'juicybliss',
+      password : '',
+      database : 'smart-brain'
+    }
+  });
+
+db.select('*').from('users').then(data => {
+    console.log(data);
+});
 
 const app = express();
 
@@ -24,17 +39,16 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password} = req.body;
-    bcrypt.hash(password, null, null, function(err, hash) {
-        console.log(hash)
-    });
-    database.users.push( {
-        id: '125',
-        name: name,
+    db('users')
+    .returning('*')
+    .insert({
         email: email,
-        entries: 0,
-        joined: new Date()   
+        name: name,
+        joined: new Date()
+    }).then(user => {
+        res.json(user[0]);
     })
-    res.json(database.users[database.users.length-1]);
+    .catch(err => res.status(400).json("Could not register."))
 })
 
 app.get('/profile/:id', (req, res) => {
