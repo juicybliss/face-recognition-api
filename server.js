1d8c9224ea208f0b1fa9515d9aef81fe1260a8d9
Elvia <etcamarena@gmail.com>
2021-09-10 23:19:18 -0700

    wrapped register endpoint functionality in a database transaction to enable user and login table info to be in sync

--- server.js
+++ server.js
@@ -39,15 +39,30 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password} = req.body;
-    db('users')
-    .returning('*')
-    .insert({
-        email: email,
-        name: name,
-        joined: new Date()
-    }).then(user => {
-        res.json(user[0]);
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+            .returning('*')
+            .insert({
+                email: loginEmail[0],
+                name: name,
+                joined: new Date()
+            })
+            .then(user => {
+                res.json(user[0]);
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
     })
+    
     .catch(err => res.status(400).json("Could not register."))
 })
 
