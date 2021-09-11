689e48f542b6fa620078dc93318d1174a94dcba2
Elvia <etcamarena@gmail.com>
2021-09-10 23:51:35 -0700

    validated credentials on the signin endpoint through DB from login

--- server.js
+++ server.js
@@ -29,12 +29,22 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email && req.body.password == database.users[0].password) {
-        res.json('it worked!')
-    } else {
-        res.status(400).json('login error');
-    }
-    
+    db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        if (isValid) {
+            return db.select('*').from('users')
+            .where('email', '=', req.body.email)
+            .then(user => {
+                res.json(user[0])
+            })
+            .catch(err => res.status(400).json('Unable to get user.'))
+        }else {
+        res.status(400).json('Wrong Credentials.')
+        }
+    })
+    .catch(err => res.status(400).json('Wrong Credentials.'))
 })
 
 app.post('/register', (req, res) => {
