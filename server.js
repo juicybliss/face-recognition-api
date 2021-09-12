f2574e9ec53711e59133671a489442e4845b878a
Elvia <etcamarena@gmail.com>
2021-09-11 17:04:53 -0700

    separating concern:moved the register functionality to its own js file in controllers dir.

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex')
+const knex = require('knex');
+
+const register = require('./controllers/register');
 
 const db = knex({
     client: 'pg',
@@ -25,7 +27,7 @@ app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => {
-    res.send(database.users)
+    res.send('Success!')
 })
 
 app.post('/signin', (req, res) => {
@@ -47,34 +49,7 @@ app.post('/signin', (req, res) => {
     .catch(err => res.status(400).json('Wrong Credentials.'))
 })
 
-app.post('/register', (req, res) => {
-    const { email, name, password} = req.body;
-    const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-            .returning('*')
-            .insert({
-                email: loginEmail[0],
-                name: name,
-                joined: new Date()
-            })
-            .then(user => {
-                res.json(user[0]);
-            })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    
-    .catch(err => res.status(400).json("Could not register."))
-})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
