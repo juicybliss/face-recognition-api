384c881b383b02072f818806bd50b156f29e38ad
Elvia <etcamarena@gmail.com>
2021-09-13 10:11:06 -0700

    fixed formatting problems instroduced by auto formatt

--- controllers/signin.js
+++ controllers/signin.js
@@ -3,25 +3,22 @@ const handleSignin = (db, bcrypt) => (req, res) => {
   if (!email || !password) {
     return res.status(400).json("incorrect form submission");
   }
-  db.select("email", "hash")
-    .from("login")
-    .where("email", "=", email)
-    .then((data) => {
+  db.select('email', 'hash').from('login')
+    .where('email', '=', email)
+    .then(data => {
       const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
-        return db
-          .select("*")
-          .from("users")
-          .where("email", "=", email)
-          .then((user) => {
-            res.json(user[0]);
+        return db.select('*').from('users')
+          .where('email', '=', email)
+          .then(user => {
+            res.json(user[0])
           })
-          .catch((err) => res.status(400).json("Unable to get user."));
+          .catch((err) => res.status(400).json('Unable to get user.'));
       } else {
-        res.status(400).json("Wrong Credentials.");
+        res.status(400).json('Wrong Credentials.');
       }
     })
-    .catch((err) => res.status(400).json("Wrong Credentials."));
+    .catch((err) => res.status(400).json(`Wrong Credentials.`));
 };
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,58 +1,40 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt-nodejs");
-const cors = require("cors");
-const knex = require("knex");
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
 
-const register = require("./controllers/register");
-const signin = require("./controllers/signin");
-const profile = require("./controllers/profile");
-const image = require("./controllers/image");
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
 
 const db = knex({
-  client: "pg",
-  connection: {
-    host: "127.0.0.1",
-    user: "juicybliss",
-    password: "",
-    database: "smart-brain"
-  }
-});
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'juicybliss',
+      password : '',
+      database : 'smart-brain'
+    }
+  });
 
-const app = express();
-// const PORT = process.env.PORT;
+  const app = express();
 
-app.use(cors());
-app.use(bodyParser.json());
-
-app.get("/", (req, res) => {
-  res.send(db.users);
-});
-app.post("/signin", signin.handleSignin(db, bcrypt));
-app.post("/register", (req, res) => {
-  register.handleRegister(req, res, db, bcrypt);
-});
-app.get("/profile/:id", (req, res) => {
-  profile.handleProfileGet(req, res, db);
-});
-app.put("/image", (req, res) => {
-  image.handleImage(req, res, db);
-});
-app.post("/imageurl", (req, res) => {
-  image.handleApiCall(req, res);
-});
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`app is running on port ${process.env.PORT}`);
-});
-
-// console.log(PORT);
-
-// app.listen(PORT, () => {
-//     console.log(`app is running on port ${PORT}`);
-// })
-
-// console.log(PORT);
+  app.use(cors())
+  app.use(bodyParser.json());
+  
+  app.get('/', (req, res)=> { res.send(db.users) })
+  app.post('/signin', signin.handleSignin(db, bcrypt))
+  app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+  app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+  app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+  app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+  
+  app.listen(3000, ()=> {
+    console.log('app is running on port 3000');
+  })
 
 /*
 Endpoints
