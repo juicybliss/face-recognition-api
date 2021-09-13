a65747b7b5efa335a3e356b95bd2634c6e9546f2
Elvia <etcamarena@gmail.com>
2021-09-12 19:47:00 -0700

    fixed security concerns with api key. cleaned up code formatting

--- controllers/image.js
+++ controllers/image.js
@@ -1,14 +1,31 @@
-const handleImage = (db) => (req, res) => {
-    const { id } = req.body;
-  db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-      res.json(entries[0])
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
-}
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "0795ddaed65f4f6c8e8d12fb1421ca1d",
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json("Unable to work with API"));
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json("unable to get entries"));
+};
 
 module.exports = {
-    handleImage: handleImage //can just use handleImage with ES6 but want to be backwards compatible.
-}
+  handleImage: handleImage, //can just use handleImage with ES6 but want to be backwards compatible.
+  handleApiCall: handleApiCall,
+};
--- controllers/profile.js
+++ controllers/profile.js
@@ -1,16 +1,18 @@
-const handleProfileGet = (db) => (req, res) => {
-        const { id } = req.params;
-        db.select('*').from('users').where({id: id})
-        .then(user => {
-            if(user.length) {
-                res.json(user[0]);
-            } else {
-                res.status(400).json("Not Found")
-            }
-        })
-        .catch(err => res.status(400).json('Error getting user'))
-    }
+const handleProfileGet = (req, res, db) => {
+  const { id } = req.params;
+  db.select("*")
+    .from("users")
+    .where({ id: id })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("Not Found");
+      }
+    })
+    .catch((err) => res.status(400).json("Error getting user"));
+};
 
 module.exports = {
-    handleProfileGet: handleProfileGet
-}
+  handleProfileGet: handleProfileGet,
+};
