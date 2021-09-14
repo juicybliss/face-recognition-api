29e43ec282f89e568fe117777ebc0b90fb960b6f
Elvia <etcamarena@gmail.com>
2021-09-14 14:05:31 -0700

    testing to see if it needs ssl disabled

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ const db = knex({
     client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
-      }
+      ssl: true
     }
   });
 
